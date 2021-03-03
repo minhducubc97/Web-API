4b2ff41c00f9a0ae049e5ce319655a25d57f77f1
Duc Nguyen <ngminhduc20111997@gmail.com>
2021-03-03 01:04:08 -0800

    Adding PUT endpoint

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ def make_json_list(csvFilePath):
     with open('classics.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            data.append({
-                'title': row[3],
-                'author': row[11],
-                'year': row[13],
-                'link': row[8]
-            })
+            if len(row) != 0:
+                data.append({
+                    'title': row[0],
+                    'author': row[2],
+                    'year': row[3],
+                    'link': row[1]
+                })
     return data
 
 
@@ -60,21 +61,57 @@ def requires_auth(f):
 
 
 @app.route('/')
-@requires_auth
 def api_root():
     return 'Welcome to Ebook free resource!'
 
 
-@app.route('/books')
-@requires_auth
-def api_articles():
+@app.route('/books', methods=['GET'])
+def api_getall():
     json_list_string = json.dumps(json_list)
     return Response(json_list_string, status=200, mimetype='application/json')
 
 
-@app.route('/books/<bookid>', methods=['GET'])
+@app.route('/books', methods=['POST'])
 @requires_auth
-def api_book(bookid):
+def api_post():
+    if request.headers['Content-Type'] == 'application/json':
+        json_request = request.json
+        row = [json_request['title'], json_request['link'],
+               json_request['author'], json_request['year']]
+        with open('classics.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(row)
+            json_list.append(json_request)
+            return "Row added to database"
+    else:
+        return 'Error 415: Unsupported media type'
+
+
+@app.route('/books/<bookid>', methods=['PUT'])
+@requires_auth
+def api_put(bookid):
+    bookid = int(bookid)
+    if (bookid >= len(json_list) or bookid < 0):
+        return 'Error 404: Book not found'
+    else:
+        json_request = request.json
+        # prevent missing properties
+        if 'title' not in json_request:
+            json_request['title'] = ''
+        if 'link' not in json_request:
+            json_request['link'] = ''
+        if 'author' not in json_request:
+            json_request['author'] = ''
+        if 'year' not in json_request:
+            json_request['year'] = ''
+        with open('classics.csv', newline='') as f:
+            read_data = [row for row in csv.DictReader(f)]
+            json_list[bookid] = json_request
+        return 'Row updated'
+
+
+@app.route('/books/<bookid>', methods=['GET'])
+def api_getbook(bookid):
     json_item = json_list[int(bookid)]
     json_item_string = json.dumps(json_item)
     return Response(json_item_string, status=200, mimetype='application/json')
